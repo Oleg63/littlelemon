89e078d9fca381262f0c8aa55cdbe14b8b2c8cf2
olsoftware-ca <106555115+olsoftware-ca@users.noreply.github.com>
2023-11-05 20:16:54 -0600

    fetching and storing

---
+++ Restaurant/Dish+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Dish+CoreDataClass.swift
+//  Restaurant
+//
+//  Created by Oleg Udovenko on 2023-11-05.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Dish)
+public class Dish: NSManagedObject {
+
+}
---
+++ Restaurant/Restaurant/MenuItemView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemView.swift
+//  Restaurant
+//
+//  Created by Oleg Udovenko on 2023-11-05.
+//
+
+import SwiftUI
+
+struct MenuItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemView()
+}
